bcc236abdd1e8db3e013cd45acb9e2509663a8ec
marine cherdo <marine.cherdo13@gmail.com>
2023-10-03 13:11:08 +0200

    remise dossier et nodemon install

---
+++ projet-design-patern/courses/index.ts
@@ -0,0 +1,40 @@
+import { Student } from './student';
+import { User, Worker, Administrator } from './worker';
+import { UnionPublisher } from "./observer";
+
+class StudentAdapter implements User {
+    
+    username: string;
+    student: Student;
+    constructor(student: Student) {
+        this.student = student;
+    }
+
+    getUsername(): string {
+        return `username: ${this.student.getFullname().toLocaleLowerCase()}`;
+    }
+
+    login(): void {
+        console.log('Im an adapted student');
+    }
+
+    notify(news: string): void {
+        console.log(news);
+    }
+}
+
+const john = new Student("john", "doe")
+
+const users: User[] = [
+    new Worker("abc123"), 
+    new Worker("vbn456"), 
+    new Administrator("admin123"),
+    new StudentAdapter(john)
+];
+
+users.forEach((user) => {
+    user.login();
+    UnionPublisher.addSubscribers(user)
+})
+
+UnionPublisher.update('Je suis un article');
