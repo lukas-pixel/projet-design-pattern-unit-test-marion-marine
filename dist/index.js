32081ba3863cffe68ccaab2df74fa67a35bfbe1b
marine cherdo <marine.cherdo13@gmail.com>
2023-10-03 10:15:59 +0200

    suite exercice

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,35 @@
-console.log('salut');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Administrator = exports.Worker = void 0;
+class Worker {
+    constructor(username) {
+        this.username = username;
+    }
+    getUsername() {
+        return this.username;
+    }
+    login() {
+        console.log("I'm doing ADMIN login stuff");
+    }
+}
+exports.Worker = Worker;
+class Administrator {
+    constructor(username) {
+        this.username = username;
+    }
+    getUsername() {
+        return "admin#" + this.username;
+    }
+    login() {
+        console.log("I'm doing ADMIN login stuff");
+    }
+    changeUsername(user, nextUsername) {
+        user.username = nextUsername;
+    }
+}
+exports.Administrator = Administrator;
+const users = [new Worker("abc123"), new Worker("vbn456"), new Administrator("admin123")];
+users.forEach((user) => {
+    user.login();
+});
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,46 @@
-console.log('salut')
+interface User {
+    username: string;
+    getUsername(): string;
+    login(): void;
+}
+
+export class Worker implements User {
+    username: string;
+
+    constructor(username: string) {
+        this.username = username;
+    }
+    getUsername() {
+        return this.username
+    }
+    login() {
+        console.log("I'm doing ADMIN login stuff");
+    }
+}
+
+export class Administrator implements User {
+
+    username: string;
+
+    constructor(username: string) {
+        this.username = username;
+    }
+
+    getUsername() {
+        return "admin#" + this.username
+    }
+
+    login() {
+        console.log("I'm doing ADMIN login stuff");
+    }
+
+    changeUsername(user: User, nextUsername: string) {
+      user.username = nextUsername;
+    }
+}
+
+const users: User[] = [new Worker("abc123"), new Worker("vbn456"), new Administrator("admin123")];
+
+users.forEach((user) => {
+    user.login();
+})
